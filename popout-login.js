9fb12a86ff935267e8e407bc7677c7c955447298
jord2097 <jordan.harrison1+git@gmail.com>
2022-06-02 23:36:33 +0100

    login state, logout func

--- popout-login.js
+++ popout-login.js
@@ -1,3 +1,9 @@
+chrome.storage.local.get(["userID"], function (result){
+    if (result.userID.length > 0) {
+        window.location.replace("./popout-success.html")
+    }    
+})
+
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
     
@@ -8,12 +14,14 @@ document.querySelector('form').addEventListener('submit', e => {
     // When user submits check if input is either blank or contains only spaces using .trim() 
     // guide in bookmarks from freecodecamp
 
-    if (username && password === "password") {
-        chrome.storage.local.set({userID: username}, function(result) {
+    if (username && password === "password") { // login
+        chrome.storage.local.set({userID: username}, function() {
+            const error = chrome.runtime.lastError
+            if (error) {
+                console.error("error")
+            }            
             console.log("Logged in as " + username)
         })        
-         window.location.replace("./popout-success.html")
-                  
-        
+        window.location.replace("./popout-success.html")       
     }
 })
