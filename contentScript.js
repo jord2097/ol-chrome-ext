9f9a2afd87df7229ba42e76e3423a93b0a00a9a0
jord2097 <jordan.harrison1+git@gmail.com>
2022-06-04 02:39:19 +0100

    timer, popup func

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,38 @@
-const init = function(){    
-    const injectElement = document.createElement('div');
-    injectElement.className = 'test-Element'
-    injectElement.innerHTML = 'INJECTED VIA CONTENT SCRIPT'
-    document.body.appendChild(injectElement)
-}
-
-init()
+chrome.storage.local.get(["userID"], function (result){
+    if (result.userID) {
+        console.log("user loggedin")
+        const renderPopup = () => {
+            console.log("timer working")
+            fetch(chrome.runtime.getURL('modal.html'))
+            .then(response => response.text())
+            .then(html => {
+                document.body.insertAdjacentHTML('beforeend', html)
+                const btn = document.getElementById("close-btn")
+                const btn2 = document.getElementById("close-btn2")
+                btn.addEventListener("click", () => {
+                    console.log("hide event")
+                    const modal = document.getElementById("help-modal")
+                    modal.style.display = "none"
+                })
+                btn2.addEventListener("click", () => {
+                    console.log("hide no event")
+                    const modal = document.getElementById("help-modal")
+                    modal.style.display = "none"
+                })
+                                           
+            })            
+            
+        }          
+        const idleTimer = () => {
+            let temp;
+            resetTimer = () => {
+                clearTimeout(temp)
+                temp = setTimeout(renderPopup, 5000)
+            }
+            window.onload = resetTimer;
+            document.onmousemove = resetTimer    
+            
+        }        
+        idleTimer()
+    }    
+})
