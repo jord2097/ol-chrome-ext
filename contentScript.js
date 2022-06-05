0877818809b9c2f0492a9ac506820e3c6d7b47c4
jord2097 <jordan.harrison1+git@gmail.com>
2022-06-05 15:42:03 +0100

    shadow DOM

--- contentScript.js
+++ contentScript.js
@@ -3,22 +3,28 @@ const renderPopup = () => {
     fetch(chrome.runtime.getURL('modal.html'))
     .then(response => response.text())
     .then(html => {
-        document.body.insertAdjacentHTML('beforeend', html)
-        /* const iframe = document.createElement('iframe')
-        iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html)
-        document.body.appendChild(iframe) */
-        const btn = document.getElementById("close-btn")
-        const btn2 = document.getElementById("close-btn2")
-        btn.addEventListener("click", () => {
-            console.log("hide after press yes")
-            const modal = document.getElementById("help-modal")
-            modal.style.display = "none"                      
+        // document.body.insertAdjacentHTML('beforeend', html)
+
+        // const iframe = document.createElement('iframe')
+        // iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html)
+        // document.body.appendChild(iframe)
+        
+        const maindiv = document.createElement('div')
+        maindiv.className = "shadow-div"
+        let shadowParent = maindiv.attachShadow({mode: 'open'})
+        shadowParent.innerHTML = `<link rel="stylesheet" type="text/css" href="${chrome.extension.getURL("output.css")}"></link>` + html        
+        document.body.appendChild(maindiv)            
+        
+        maindiv.addEventListener("click", () => {
+            console.log("hide after press yes/no")
+            // const modal = document.getElementById("help-modal")
+            maindiv.style.display = "none"                      
         })
-        btn2.addEventListener("click", () => {
-            console.log("hide after press no")
-            const modal = document.getElementById("help-modal")
-            modal.style.display = "none"                      
-        })                                   
+        // btn.addEventListener("click", () => {
+        //     console.log("hide after press no")
+        //     const modal = document.getElementById("help-modal")
+        //     modal.style.display = "none"                      
+        // })                                   
     })
     
     
